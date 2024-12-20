8c48c8c304b887b7775409c0a7781ad8316e1ba3
Habibi Mustafa <habibi@dicoding.com>
2024-12-20 14:50:49 +0700

    clean build tag

--- dynobuild.sh
+++ dynobuild.sh
@@ -7,20 +7,19 @@ echo_cyan() {
   echo -e "${CYAN}$@${NC}"
 }
 
-echo_cyan => "Install requirement packages"
+echo_cyan "=> Install requirement packages"
 apt update
-apt install -y build-essential cmake autoconf libtool gcc automake openssl pkg-config libyaml-dev libssl-dev
+apt install -y build-essential git cmake autoconf libtool gcc automake openssl pkg-config libyaml-dev libssl-dev
 
 echo
-echo_cyan => "Clone dynomite repository"
+echo_cyan "=> Clone dynomite repository"
 cd /tmp
 git clone https://github.com/Netflix/dynomite.git
 cd /tmp/dynomite
 git checkout v0.6.22
 
 echo
-echo_cyan => "Fix dyn_ring_queue code"
-# Reference: https://github.com/Netflix/dynomite/pull/775/files
+echo_cyan "=> Fix dyn_ring_queue code"
 cd /tmp/dynomite
 sed -i '14i _C2G_InQ C2G_InQ = {};' src/dyn_ring_queue.c 
 sed -i '15i _C2G_OutQ C2G_OutQ = {};' src/dyn_ring_queue.c 
@@ -35,26 +34,35 @@ git add .
 git commit -m "Fix building and linking with GCC 10"
 
 echo
-echo_cyan => "Unzip yaml library"
+echo_cyan "=> Unzip yaml library"
 cd /tmp/dynomite/contrib
 tar -xzf yaml-0.1.4.tar.gz
 
 echo
-echo_cyan => "Run autoupdate conf"
+echo_cyan "=> Run autoupdate conf"
 cd /tmp/dynomite/contrib/yaml-0.1.4
 autoupdate
 cd /tmp/dynomite
 autoupdate
+git add .
+git commit -m "autoupdate conf"
 
 echo
-echo_cyan => "Run build"
+echo_cyan "=> Cleaning build tag"
+git tag "v0.6.23"
+git checkout v0.6.23
+git reset --hard HEAD
+
+echo
+echo_cyan "=> Run build"
 cd /tmp/dynomite
 autoreconf -fvi
 ./configure --enable-debug=yes
+make clean
 make
 src/dynomite -h
 
 echo
-echo_cyan => "Clean up"
+echo_cyan "=> Clean up"
 cp /tmp/dynomite/src/dynomite "$(pwd)/dynomite"
 rm -r /tmp/dynomite
